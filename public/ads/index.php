9ed2be2f96fb391ca4dbf320e355c9b45433b43e
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-23 00:39:08 -0500

    wrote pagination function

--- public/ads/index.php
+++ public/ads/index.php
@@ -16,17 +16,11 @@ require_once __DIR__ . "../../../bootstrap.php";
 		<section class="row text-center">
 			<?php foreach ($ads as $ad): ?>
 				<div class= "col-md-12 panel panel-default">
-					<h4><?= Ads::all($singleAd->id) ?>
-					<h4><?= Ads::all($singleAd->user_id) ?>
-					<h4><?= Ads::all($singleAd->username) ?>
-					<h4><?= Ads::all($singleAd->date_posted) ?>
-					<h4><?= Ads::all($singleAd->category) ?>
-					<h4><?= Ads::all($singleAd->price) ?>
-					<h4><?= Ads::all($singleAd->description) ?>
-					<h4><?= Ads::all($singleAd->availability_status) ?>
-					<h4><?= Ads::all($singleAd->pick_up_location) ?>
-					<h4><?= Ads::all($singleAd->location_city) ?>
-					<!-- <h4><?= Ads::all($singleAd->image) ?> -->
+					<h4>Name:</h4>
+					<h3>Price:</h3>
+					<h3>Date Posted:</h3>
+					<img src="">
+					<h2>Category: ()</h2>
 				</div>
 			<?php endforeach ?>
 		</section>
