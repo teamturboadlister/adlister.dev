b8a7c9118bb19693e6a04db6928ffc9b22e30471
Gonzalo Rodriguez <g@stm.pm>
2017-08-23 10:08:37 -0500

    formated the ads view

--- public/ads/index.php
+++ public/ads/index.php
@@ -7,11 +7,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ads</title>
+  <head>
+  			<meta charset="utf-8">
+        <title>Ads</title>
+  		<link rel="stylesheet" type="text/css" href="../css/menu.css">
+  		<link rel="stylesheet" type="text/css" href="../css/main.css">
+  		<link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+
+
 </head>
 <body>
+	<?php include '../assets/menu.php';?>
+	      <!-- menu panel -->
+	      <div id="content">
+	         <div class="menu-trigger"></div>
+	         <!-- header panel -->
+	         <?php include '../assets/header.php';?>
+	         <!-- Content panel -->
+
+	         <div class="container center-div">
+	<div class="container">
 	<h1>full list of items for sale</h1>
 	<div>
+    <div class="row">
 		<section class="row text-center">
 			<?php foreach ($ads as $ad): ?>
 				<div class= "col-md-12 panel panel-default">
@@ -23,6 +41,13 @@
 				</div>
 			<?php endforeach ?>
 		</section>
-	</div>
-</body>
+  </div>
+  </div>
+            <?php include '../assets/footer.php' ?>
+
+         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+         <script type="text/javascript" src="../js/main.js"></script>
+         <script type="text/javascript" src="../js/menu.js"></script>
+      </body>
 </html>
